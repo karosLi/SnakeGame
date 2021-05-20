eb9910ddff811a04eabd613c92c38ef7188d6a29
likai <likai@wepie.com>
2021-05-20 14:52:47 +0800

    增加部分注释

--- OpenGLEnv/snakeGame/objects/game_object.cpp
+++ OpenGLEnv/snakeGame/objects/game_object.cpp
@@ -8,10 +8,10 @@
 #include "game_object.h"
 
 GameObject::GameObject()
-    : FrameIndex(0), Position(0, 0), StaticPosition(0, 0), Size(1, 1), Velocity(0.0f), Color(1.0f), Rotation(0.0f), RotationQuat(glm::mat4(1.0f)), Sprite(), IsSolid(false), Destroyed(false) { }
+    : FrameIndex(0), Position(0, 0), Size(1, 1), Velocity(0.0f), Color(1.0f), Rotation(0.0f), RotationQuat(glm::mat4(1.0f)), Sprite(), IsSolid(false), Destroyed(false) { }
 
 GameObject::GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec4 color, glm::vec2 velocity)
-    : FrameIndex(0), Position(pos), StaticPosition(0, 0), Size(size), Velocity(velocity), Color(color), Rotation(0.0f), RotationQuat(glm::mat4(1.0f)), Sprite(sprite), IsSolid(false), Destroyed(false) { }
+    : FrameIndex(0), Position(pos), Size(size), Velocity(velocity), Color(color), Rotation(0.0f), RotationQuat(glm::mat4(1.0f)), Sprite(sprite), IsSolid(false), Destroyed(false) { }
 
 void GameObject::Draw(SpriteRenderer &renderer)
 {
--- OpenGLEnv/snakeGame/objects/game_object.h
+++ OpenGLEnv/snakeGame/objects/game_object.h
@@ -25,14 +25,14 @@ class GameObject
 public:
     // Object state
     GLint       FrameIndex;// 对象当前渲染的帧索引
-    glm::vec2   Position, StaticPosition, Size, Velocity;
-    glm::vec4   Color;
-    GLfloat     Rotation;
-    glm::quat   RotationQuat;
+    glm::vec2   Position, Size, Velocity;// 位置，大小，每秒速度
+    glm::vec4   Color;// 颜色
+    GLfloat     Rotation;// 旋转角度，正数为顺时针，负数为逆时针
+    glm::quat   RotationQuat;// 旋转四元数
     GLboolean   IsSolid;// 不可被摧毁
-    GLboolean   Destroyed;
+    GLboolean   Destroyed;// 是否被销毁
     // Render state
-    Texture2D   Sprite;
+    Texture2D   Sprite;// 纹理精灵
     // Constructor(s)
     GameObject();
     GameObject(glm::vec2 pos, glm::vec2 size, Texture2D sprite, glm::vec4 color = glm::vec4(1.0f), glm::vec2 velocity = glm::vec2(0.0f, 0.0f));
